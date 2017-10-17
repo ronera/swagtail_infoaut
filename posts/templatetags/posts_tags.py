e60dcc41f66187407e5c268ac39e787048d1818b
Arenor <arenor@protonmail.com>
2017-10-17 19:28:12 +0200

    templatetags: aggiungiamo un tag per gli eventi

--- posts/templatetags/posts_tags.py
+++ posts/templatetags/posts_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from posts.models import PostCategory
+from posts.models import PostCategory, PostPage
 
 register = template.Library()
 
@@ -7,5 +7,17 @@ register = template.Library()
 def categories(context):
     return {
         'categories': PostCategory.objects.all(),
+        'request': context['request'],
+    }
+
+@register.inclusion_tag('posts/tags/events.html', takes_context=True)
+def events(context):
+    events_posts = PostPage.objects.live().order_by(
+        '-first_published_at',
+    ).filter(
+        categorie__name="Agenda",
+    )
+    return {
+        'events_posts': events_posts,
         'request': context['request'],
     }
